26d50c0328ff6a06613c1d7b7e8ecdadccbc4f3f
FIlipeAugusto <FIlipeAugusto@10.1.6.95>
2017-03-31 14:05:41 -0300

    Atestado de Matrícula e Frequência

---
+++ erudio-server/src/ReportBundle/Controller/MatriculaReportController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ReportBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Ps\PdfBundle\Annotation\Pdf;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class MatriculaReportController extends Controller {
+    
+    function getMatriculaFacade() {
+        return $this->get('facade.matricula.matriculas');
+    }
+    
+    function getEnturmacaoFacade() {
+        return $this->get('facade.matricula.enturmacoes');
+    }
+    
+    /**
+    * @ApiDoc(
+    *   resource = true,
+    *   section = "Módulo Relatórios",
+    *   description = "Atestado de Matrícula",
+    *   statusCodes = {
+    *       200 = "Documento PDF"
+    *   }
+    * )
+    * 
+    * @Route("atestados/atestado-matricula", defaults={ "_format" = "pdf" })
+    * @Pdf(stylesheet = "reports/templates/stylesheet.xml")
+    */
+    function atestadoAction(Request $request) {
+        try {
+           $matricula = $this->getMatriculaFacade()->find($request->query->getInt('matricula'));
+           $enturmacoes = $matricula->getEnturmacoesAtivas();
+           $etapa = count($enturmacoes) 
+                   ? $enturmacoes->first()->getTurma()->getEtapa()->getNomeExibicao() 
+                   : 'Não Enturmado';
+            return $this->render('reports/atestado/matricula.pdf.twig', [
+                'instituicao' => $matricula->getUnidadeEnsino(),
+                'matricula' => $matricula,
+                'etapa' => $etapa
+            ]);
+        } catch (\Exception $ex) {
+            $this->get('logger')->error($ex->getMessage());
+            return new Response($ex->getMessage(), 500);
+        }
+    }
+}
