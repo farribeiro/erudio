6b43e53523c27850f2ae1a16ec772cbc9f0d5c77
07800287912 <jhony.almeida@edu.itajai.sc.gov.br>
2017-10-11 08:09:07 -0300

    Ajustes, adição do campo país de origem em pessoa e de filtro em professores de disciplina ofertada

--- erudio-server/src/PessoaBundle/Entity/PessoaFisica.php
+++ erudio-server/src/PessoaBundle/Entity/PessoaFisica.php
@@ -86,6 +86,13 @@ class PessoaFisica extends Pessoa {
     */
     private $nacionalidade;
     
+    /** 
+    * @JMS\Groups({"DETAILS"})
+    * @ORM\ManyToOne(targetEntity = "Pais") 
+    * @ORM\JoinColumn(name = "pais_nacionalidade_id", referencedColumnName = "id") 
+    */
+    private $paisOrigem;
+    
     /**
     * @JMS\Groups({"DETAILS"})
     * @ORM\Column(name = "certidao_nascimento_completa", length = 32) 
@@ -244,11 +251,14 @@ class PessoaFisica extends Pessoa {
     function getNacionalidade() {
         return $this->nacionalidade;
     }
+    
+    function getPaisOrigem() {
+        return $this->paisOrigem;
+    }
 
     function getCertidaoNascimento() {
         return $this->certidaoNascimento;
     }
-    
         
     function getDataExpedicaoCertidaoNascimento() {
         return $this->dataExpedicaoCertidaoNascimento;
@@ -324,6 +334,10 @@ class PessoaFisica extends Pessoa {
 
     function setNacionalidade($nacionalidade) {
         $this->nacionalidade = $nacionalidade;
+    }
+    
+    function setPaisOrigem($paisOrigem) {
+        $this->paisOrigem = $paisOrigem;
     }
 
     function setCertidaoNascimento($certidaoNascimento) {
